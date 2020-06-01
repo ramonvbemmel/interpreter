aefa5bf5d53445ee365a846ed9b15b41ce143432
Ramon <ramon.vanbemmel@student.hu.nl>
2020-06-01 16:18:42 +0200

    removed loop counter

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from interpreter import interper, evaluate_loop_statement
 filename= input ("Geef de naaam en/of path van uw source file:" )
 mylist=run_tokenizer("voorbeeld.txt")
 interper(mylist)
-print("Loop statement ", evaluate_loop_statement.counter, "times evaluated")
+# print("Loop statement ", evaluate_loop_statement.counter, "times evaluated")
